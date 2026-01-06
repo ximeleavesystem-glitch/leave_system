4e1fbb8e041e8c8a10f5592fb96e8a4d924d9233
ximeleavesystem-glitch <xime.leave.system@gmail.com>
2026-01-06 22:12:42 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-// js/config.js
-export const APP_CONFIG = {
-  rosterApiUrl: "https://script.google.com/macros/s/AKfycbyrkVkBnEChSlKEdZHxR4msHxZKz21FiLOUx3TwIr2sCOh61KV08cCirXHzxthZCBy8_w/exec",
-  deanEmail: "deanacademicchennai@xime.org",
-  defaultAvatar:
-    "https://www.gravatar.com/avatar/?d=mp&f=y" // simple fallback
-};
